cc3f6b1258e9b42605e71cbe651ffef3789de30b
Martín Ponce <martinp88@gmail.com>
2023-02-11 17:12:16 -0300

    - Moved Firebase config to .env file. - Added error handling in todos.js

--- firestore.js
+++ firestore.js
@@ -1,7 +1,19 @@
+require('dotenv').config();
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 
-var serviceAccount = require("./pon-todo-list-firebase-adminsdk-k425p-ab169c2299.json");
+var serviceAccount = {
+  "type": process.env.TYPE,
+  "project_id": process.env.PROJECT_ID,
+  "private_key_id": process.env.PRIVATE_KEY_ID,
+  "private_key": process.env.PRIVATE_KEY,
+  "client_email": process.env.CLIENT_EMAIL,
+  "client_id": process.env.CLIENT_ID,
+  "auth_uri": process.env.AUTH_URI,
+  "token_uri": process.env.TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
+};
 
 initializeApp({
   credential: cert(serviceAccount)
--- todos.js
+++ todos.js
@@ -3,108 +3,144 @@ const router = express.Router();
 const db = require('./firestore');
 
 router.get('/', async (req, res) => {
-  let todos = [];
-  let snapshot = await db.collection('todos').get();
-  if (!snapshot.empty) {
-    snapshot.forEach(doc => {
-      todos.push({
-        id: doc.id,
-        ...doc.data()
-      });
+  try {
+    let todos = [];
+    let snapshot = await db.collection('todos').get();
+    if (!snapshot.empty) {
+      snapshot.forEach(doc => {
+        todos.push({
+          id: doc.id,
+          ...doc.data()
+        });
+      })
+    }
+
+    return res.json({
+      todos
+    })
+  } catch (e) {
+    return res.status(500).json({
+      message: 'Internal server error',
+      error: e
     })
   }
-
-  return res.json({
-    todos
-  })
 })
 
 router.get('/:id', async (req, res) => {
-  let id = req.params.id;
-  let doc = await db.collection('todos').doc(id).get();
+  try {
+    let id = req.params.id;
+    let doc = await db.collection('todos').doc(id).get();
 
-  if (!doc.exists) {
-    return res.status(404).json({
-      message: `To Do with id ${id} doesn't exist!`
+    if (!doc.exists) {
+      return res.status(404).json({
+        message: `To Do with id ${id} doesn't exist!`
+      })
+    }
+
+    let todo = {
+      id: doc.id,
+      ...doc.data()
+    }
+
+    return res.json({
+      todo
+    });
+  } catch (e) {
+    return res.status(500).json({
+      message: 'Internal server error',
+      error: e
     })
   }
-
-  let todo = {
-    id: doc.id,
-    ...doc.data()
-  }
-
-  return res.json({
-    todo
-  });
 })
 
 router.post('/', async (req, res) => {
-  let description = req.body.description;
+  try {
 
-  if (description == '') {
-    return res.status(400).json({
-      message: 'Description cannot be empty'
-    });
+    let description = req.body.description;
+
+    if (description == '') {
+      return res.status(400).json({
+        message: 'Description cannot be empty'
+      });
+    }
+
+    let todo = {
+      description,
+      done: false
+    }
+
+    inserted = await db.collection('todos').add(todo);
+    todo.id = inserted.id
+
+    return res.json({
+      todo
+    })
+  } catch (e) {
+    return res.status(500).json({
+      message: 'Internal server error',
+      error: e
+    })
   }
-
-  let todo = {
-    description,
-    done: false
-  }
-
-  inserted = await db.collection('todos').add(todo);
-  todo.id = inserted.id
-
-  return res.json({
-    todo
-  })
 });
 
 router.put('/:id', async (req, res) => {
-  let id = req.params.id;
-  let description = req.body.description;
-  let done = req.body.done;
-  let docRef = db.collection('todos').doc(id);
-  let doc = await docRef.get();
+  try {
+    let id = req.params.id;
+    let description = req.body.description;
+    let done = req.body.done;
+    let docRef = db.collection('todos').doc(id);
+    let doc = await docRef.get();
 
-  if (!doc.exists) {
-    return res.status(404).json({
-      message: `To Do with id ${id} doesn't exist!`
+    if (!doc.exists) {
+      return res.status(404).json({
+        message: `To Do with id ${id} doesn't exist!`
+      })
+    }
+
+    await docRef.set({
+      description,
+      done
+    })
+
+    doc = await docRef.get();
+
+    let todo = {
+      id: doc.id,
+      ...doc.data()
+    }
+    return res.json({
+      todo
+    });
+  } catch (e) {
+    return res.status(500).json({
+      message: 'Internal server error',
+      error: e
     })
   }
-
-  await docRef.set({
-    description,
-    done
-  })
-
-  doc = await docRef.get();
-
-  let todo = {
-    id: doc.id,
-    ...doc.data()
-  }
-  return res.json({
-    todo
-  });
 });
 
 router.delete('/:id', async (req, res) => {
-  let id = req.params.id;
-  let docRef = await db.collection('todos').doc(id)
-  let doc = await docRef.get();
+  try {
+    let id = req.params.id;
+    let docRef = await db.collection('todos').doc(id)
+    let doc = await docRef.get();
 
-  if (!doc.exists) {
-    return res.status(404).json({
-      message: `To Do with id ${id} doesn't exist!`
+    if (!doc.exists) {
+      return res.status(404).json({
+        message: `To Do with id ${id} doesn't exist!`
+      })
+    }
+    await docRef.delete();
+
+    return res.json({
+      message: 'Deleted successfully!'
+    });
+  } catch (e) {
+    return res.status(500).json({
+      message: 'Internal server error',
+      error: e
     })
   }
-  await docRef.delete();
-
-  return res.json({
-    message: 'Deleted successfully!'
-  });
 })
 
 module.exports = router;
